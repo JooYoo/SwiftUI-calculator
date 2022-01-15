725a6640ab29cce300defea68cfb79d55c71c6c5
JooYoo <jooyoo@outlook.com>
2022-01-15 11:24:51 +0100

    Initial Commit

---
+++ swiftui-calculator/swiftui_calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_calculatorApp.swift
+//  swiftui-calculator
+//
+//  Created by Yu on 2022/1/15.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
