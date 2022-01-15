c4df721ae2ca2af97aa976caffa94ae45ef1b77d
JooYoo <jooyoo@outlook.com>
2022-01-15 11:47:18 +0100

    chore: commit test from Xcode

--- swiftui-calculator/ContentView.swift
+++ swiftui-calculator/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world")
+        Text("Hello, from Xcode")
             .padding()
     }
 }
@@ -17,5 +17,7 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 12")
+            .previewDisplayName("iPhone 12")
     }
 }
